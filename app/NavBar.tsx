04d8d6f6e5c07ca7c703bea8483d2ad77526a229
kianibound <kianibound@gmail.com>
2023-10-16 21:18:10 +0330

    style navbar active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,43 @@
-import Link from "next/link"
-import {TbEaseInOutControlPoints} from "react-icons/tb"
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { TbEaseInOutControlPoints } from "react-icons/tb";
+import classnames from "classnames";
 
 const NavBar = () => {
-    const links =[
-        {label : 'Dashbaord', href : '/'},
-        {label : 'Issues', href : '/issues'}
-    ]
+  const currentPath = usePathname()
+
+  const links = [
+    {
+      label: "Dashbaord", href: "/"
+    },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 h-14 px-5 items-center">
-        <Link href='/' className="text-2xl"><TbEaseInOutControlPoints /></Link>
-        <ul className="flex space-x-6">
-        {
-            links.map(link =>
-                <Link className="text-zinc-500 hover:text-zinc-800 transition-colors" key={link.href} href={link.href}>{link.label}</Link>
-                )
-        }
-        </ul>
+      <Link href="/" className="text-2xl">
+        <TbEaseInOutControlPoints />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            className={
+              classnames({
+                'text-zinc-900 font-bold': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+              })
+            }
+            key={link.href}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
